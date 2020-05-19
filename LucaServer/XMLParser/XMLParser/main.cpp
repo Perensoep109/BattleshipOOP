4516d616931e62003bc46d1475fccc22464f8983
Luca Stronks <lucastronks@gmail.com>
2020-05-19 23:50:46 +0200

    Added basic functionality for opening files to the XML parser.

--- LucaServer/XMLParser/XMLParser/main.cpp
+++ LucaServer/XMLParser/XMLParser/main.cpp
@@ -1,7 +1,23 @@
 #include <iostream>
+#include <fstream>
 
 int main()
 {
-	std::cout << "Hello, world" << std::endl;
-	return 0;
+	std::fstream file;
+	std::string text = "Hello, world";
+	
+	file.open("settings.xml");
+	
+	if (file.is_open())
+	{
+		// Write data to file.
+		file << text.c_str();
+		file.close();
+		return 0;
+	}
+	else
+	{
+		std::cout << "Error opening file.";
+		return 1;
+	}
 }
