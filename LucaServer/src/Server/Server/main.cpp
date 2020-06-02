d020ba73a0f3f746d91b6242eca60770efca5b9e
Luca Stronks <lucastronks@gmail.com>
2020-06-02 20:50:32 +0200

    Added logging functionality

--- LucaServer/src/Server/Server/main.cpp
+++ LucaServer/src/Server/Server/main.cpp
@@ -1,15 +1,121 @@
 #include <iostream>
+#include <sstream>
 #include <WS2tcpip.h>
 
-// Link Ws2_32.lib.
-#pragma comment (lib, "Ws2_32.lib")
+class Log
+{
+private:
+	bool m_traceEnabled = false;
+	bool m_warningEnabled = false;
+	bool m_errorEnabled = false;
+	bool m_fatalEnabled = false;
+	bool m_verboseEnabled = false;
 
-#define PORT "1234"
-#define DEFAULT_BUFLEN 512
+public:
+	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+
+	void SetLogLevel(const char* level)
+	{
+		std::string enteredLevel = level;
+		
+		for (int i = 0; i < sizeof(enteredLevel); i++)
+		{
+			enteredLevel[i] = tolower(enteredLevel[i]);
+		}
+
+		if (enteredLevel.find("none") != std::string::npos)		{ return; }
+		if (enteredLevel.find("trace") != std::string::npos)	{ m_traceEnabled = true; }
+		if (enteredLevel.find("warning") != std::string::npos)	{ m_warningEnabled = true; }
+		if (enteredLevel.find("error") != std::string::npos)	{ m_errorEnabled = true; }
+		if (enteredLevel.find("fatal") != std::string::npos)	{ m_fatalEnabled = true; }
+		if (enteredLevel.find("verbose") != std::string::npos)	{ m_verboseEnabled = true; }
+	}
+
+
+	void Trace(const char* message)
+	{
+		if (m_traceEnabled || m_verboseEnabled)
+		{
+			SetConsoleTextAttribute(hConsole, 10);
+			std::cout << "[INFO]: " << message << std::endl;
+			SetConsoleTextAttribute(hConsole, 7);
+		}
+	}
+
+	void Warning(const char* message)
+	{
+		if (m_warningEnabled || m_verboseEnabled)
+		{
+			SetConsoleTextAttribute(hConsole, 14);
+			std::cout << "[WARNING]: " << message << std::endl;
+			SetConsoleTextAttribute(hConsole, 7);
+		}
+	}
+
+	void Error(const char* message)
+	{
+		if (m_errorEnabled || m_verboseEnabled)
+		{
+			SetConsoleTextAttribute(hConsole, 13);
+			std::cout << "[ERROR]: " << message << std::endl;
+			SetConsoleTextAttribute(hConsole, 7);
+		}
+	}
+	
+	void Fatal(const char* message)
+	{
+		if (m_fatalEnabled || m_verboseEnabled)
+		{
+			SetConsoleTextAttribute(hConsole, 12);
+			std::cout << "[FATAL]: " << message << std::endl;
+			SetConsoleTextAttribute(hConsole, 7);
+		}
+	}
+};
+
+class Player
+{
+public:
+	int boardX;
+	int boardY;
+
+	// For now, work only with a 10x10 game board.
+	int gameBoard[100];
+
+	void getBoard(int receivedGameBoard[])
+	{
+		// Store the copy of the player's game board you received from the client.
+		for (int i = 0; i < sizeof(receivedGameBoard); i++)
+		{
+			gameBoard[i] = receivedGameBoard[i];
+		}
+	}
+};
+
+class Game
+{
+
+};
 
 int main()
 {
-	std::cout << "STARTING SERVER..." << std::endl;
+	Log log;
+	log.SetLogLevel("trace warning error fatal");
+
+	/*log.Trace("This is a Trace message");
+	log.Warning("This is a Warning message");
+	log.Error("This is an Error message");
+	log.Fatal("This is a Fatal message");*/
+
+	log.Trace("STARTING SERVER...");
+
+	std::cout << "Server is starting..." << std::endl;
+
+	// Link Ws2_32.lib.
+#pragma comment (lib, "Ws2_32.lib")
+
+	const char* PORT = "1234";
+	const int DEFAULT_BUFLEN = 512;
 
 	WSADATA wsaData;
 	int iResult;
@@ -25,15 +131,14 @@ int main()
 	int recvbuflen = DEFAULT_BUFLEN;
 
 	// Initialize Winsock.
-	std::cout << "WINSOCK ";
 	iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
 	if (iResult != 0)
 	{
-		std::cout << "ERROR: " << iResult << std::endl;
+		log.Error("INITIALIZE WINSOCK FAILED: " + iResult);
 		return 1;
 	}
 	else
-		std::cout << "OK" << std::endl;
+		log.Trace("INITIALIZE WINSOCK OK");
 
 	ZeroMemory(&hints, sizeof(hints));
 	hints.ai_family = AF_INET;
@@ -42,73 +147,79 @@ int main()
 	hints.ai_flags = AI_PASSIVE;
 
 	// Resolve the server address and port (host).
-	std::cout << "RESOLVE HOST ";
 	iResult = getaddrinfo(NULL, PORT, &hints, &result);
 	if (iResult != 0)
 	{
-		std::cout << "ERROR: " << iResult << std::endl;
+		log.Error("RESOLVE HOST FAILED: " + iResult);
 	}
 	else
-		std::cout << "OK" << std::endl;
+		log.Trace("RESOLVE HOST OK");
 
 	// Create a SOCKET for connecting to server.
-	std::cout << "CREATE SOCKET ";
 	ListenSocket = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
 	if (ListenSocket == INVALID_SOCKET)
 	{
-		std::cout << "ERROR: " << WSAGetLastError() << std::endl;
+		std::stringstream logString;
+		logString << "CREATE SOCKET FAILED: " << WSAGetLastError();
+		log.Error(logString.str().c_str());
 		freeaddrinfo(result);
 		WSACleanup();
 		return 1;
 	}
 	else
-		std::cout << "OK" << std::endl;
+		log.Trace("CREATE SOCKET OK");
 
 	// Setup the TCP listening socket.
-	std::cout << "BIND SOCKET ";
 	iResult = bind(ListenSocket, result->ai_addr, (int)result->ai_addrlen);
 	if (iResult == SOCKET_ERROR)
 	{
-		std::cout << "ERROR: " << WSAGetLastError() << std::endl;;
+		std::stringstream logString;
+		logString << "BIND SOCKET FAILED: " << WSAGetLastError();
+		log.Error(logString.str().c_str());
+
 		freeaddrinfo(result);
 		closesocket(ListenSocket);
 		WSACleanup();
 		return 1;
 	}
 	else
-		std::cout << "OK" << std::endl;
+		log.Trace("BIND SOCKET OK");
 
 	freeaddrinfo(result);
 
-	std::cout << "SET LISTENER ";
 	iResult = listen(ListenSocket, SOMAXCONN);
 	if (iResult == SOCKET_ERROR)
 	{
-		std::cout << "ERROR: " << WSAGetLastError() << std::endl;
+		std::stringstream logString;
+		logString << "SET LISTENER FAILED: " << WSAGetLastError();
+		log.Error(logString.str().c_str());
 		closesocket(ListenSocket);
 		WSACleanup();
 		return 1;
 	}
 	else
-		std::cout << "OK" << std::endl;
+		log.Trace("SET LISTENER OK");
 
 	// Accept the cliet socket.
-	std::cout << "ACCEPT ";
 	ClientSocket = accept(ListenSocket, NULL, NULL);
 	if (ClientSocket == INVALID_SOCKET)
 	{
-		std::cout << "ERROR: " << WSAGetLastError() << std::endl;
+		std::stringstream logString;
+		logString << "ACCEPT CLIENT FAILED: " << WSAGetLastError();
+		log.Error(logString.str().c_str());
 		closesocket(ListenSocket);
 		WSACleanup();
 		return 1;
 	}
 	else
-		std::cout << "OK" << std::endl;
+		log.Trace("ACCEPT CLIENT OK");
 
 	// No longer need server socket, yeet it.
 	closesocket(ListenSocket);
 
-	std::cout << "SERVER OK" << std::endl;
+	log.Trace("SERVER STARTUP COMPLETE");
+
+	std::cout << "Startup successful" << std::endl;
 
 	// Receive until the peer shuts down the connection.
 	do {
@@ -130,7 +241,7 @@ int main()
 		}
 		else if (iResult == 0)
 			std::cout << "CLIENT DISCONNECTED" << std::endl;
-		else 
+		else
 		{
 			std::cout << "RECEIVE FAILED" << WSAGetLastError() << std::endl;
 			closesocket(ClientSocket);
@@ -140,11 +251,12 @@ int main()
 	} while (iResult > 0);
 
 	// Shutdown connection.
-	std::cout << "SHUTDOWN ";
+	log.Trace("SHUTDOWN COMMAND RECEIVED");
+	std::cout << "SHUTTING DOWN" << std::endl;
 	iResult = shutdown(ClientSocket, SD_SEND);
 	if (iResult == SOCKET_ERROR)
 	{
-		std::cout << "ERROR: I'm sorry, Dave. I'm afraid I can't do that." << std::endl;
+		log.Error("SHUTDOWN FAILED");
 		closesocket(ClientSocket);
 		WSACleanup();
 		return 1;
@@ -154,6 +266,6 @@ int main()
 	closesocket(ClientSocket);
 	WSACleanup();
 
-	std::cout << "OK" << std::endl;
+	log.Trace("SHUTDOWN COMPLETE");
 	return 0;
 }
